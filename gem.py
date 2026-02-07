3e7470ccb3cc84a96c8a0f894bc5790c47b29d44
Ali Hussein <ah0979075074@gmail.com>
2026-02-07 19:53:59 +0300

    put the agent in a function

--- gem.py
+++ gem.py
@@ -57,94 +57,100 @@ function_declaration=[
 #Tool object
 tool = types.Tool(function_declarations=function_declaration)
 
-user_prompt=input("What do you want to do: ")
+
 
 #Agent loop
-history= [
-    types.Content(
-        role="user",
-        parts=[
-            types.Part.from_text(text=user_prompt)
-        ]
-    )
-]
-
-system_message = """
-You are an expert study assistant helping a student prepare for an upcoming exam.
-The student is in a hurry and wants focused, efficient help.
-
-Rules:
-- You only assist with studying-related requests.
-- If a request does not require a tool, answer directly.
-- Answer only using well-known facts or clearly state when you are unsure.
-- Do not invent information or assume missing details.
-- If a tool is required but one or more arguments are missing, ask the student to provide them.
-- Do not fill in missing tool arguments yourself.
-- Call at most one tool per turn.
-"""
-
-system_inst = types.Content(
-    parts=[types.Part.from_text(text=system_message)]
-)
-
-while True:
-    response= client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=history,
-        config=types.GenerateContentConfig(
-            system_instruction=system_inst,
-            tools=[tool]
+def study_agent(user_prompt):
+    tools_called=[]
+    history= [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=user_prompt)
+            ]
         )
-    )
-
-    candidate = response.candidates[0]
-    content = candidate.content
-    history.append(content)
-
-    function_calls = [
-        part.function_call
-        for part in content.parts
-        if part.function_call is not None
     ]
 
-    if not function_calls:
-        print("\nFinal Answer:\n")
-        final_text = "".join(
-        part.text for part in content.parts if part.text
-        )
-        print(final_text)
-        break
+    system_message = f"""
+    You are an expert study assistant helping a student prepare for an upcoming exam.
+    The student is in a hurry and wants focused, efficient help.
 
-    for fc in function_calls:
-        func_name = fc.name
-        args = dict(fc.args)
+    Rules:
+    - You only assist with studying-related requests.
+    - If a request does not require a tool, answer directly.
+    - Answer only using well-known facts or clearly state when you are unsure.
+    - Do not invent information or assume missing details.
+    - If a tool is required but one or more arguments are missing, ask the student to provide them.
+    - Do not fill in missing tool arguments yourself.
+    - Call at most one tool per turn.
+    - The user may provide the chapter file or may give nothing/empty string
+    - The user may provide the question file or may give nothing/empty string
+    """
 
-        print(f"\nCalling tool: {func_name}")
-        print("Arguments:", args)
+    system_inst = types.Content(
+        parts=[types.Part.from_text(text=system_message)]
+    )
 
-        try:
-            if func_name not in tools_map:
-                raise RuntimeError(f"Unknown tool requested: {func_name}")
-            result = tools_map[func_name](**args)
-            result_str = json.dumps({"result": result})  # usually JSON string
-        except Exception as e:
-            result_str = json.dumps({"error": str(e)})
-
-        print("Tool result:", result_str)
-
-        # Append function response to history
-        history.append(
-            types.Content(
-                role="tool",
-                parts=[
-                    types.Part(
-                        function_response=types.FunctionResponse(
-                            name=func_name,
-                            response={"result": result}  
-                        )
-                    )
-                ]
+    while True:
+        response= client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=history,
+            config=types.GenerateContentConfig(
+                system_instruction=system_inst,
+                tools=[tool]
             )
-
         )
+
+        candidate = response.candidates[0]
+        content = candidate.content
+        history.append(content)
+
+        function_calls = [
+            part.function_call
+            for part in content.parts
+            if part.function_call is not None
+        ]
+
+        if not function_calls:
+            print("\nFinal Answer:\n")
+            final_text = "".join(
+            part.text for part in content.parts if part.text
+            )
+            print(final_text)
+            return{"final_answer": final_text, "tools_called":tools_called}
+            break
+
+        for fc in function_calls:
+            func_name = fc.name
+            args = dict(fc.args)
+
+            print(f"\nCalling tool: {func_name}")
+            print("Arguments:", args)
+            tools_called.append(func_name)
+
+            try:
+                if func_name not in tools_map:
+                    raise RuntimeError(f"Unknown tool requested: {func_name}")
+                result = tools_map[func_name](**args)
+                result_str = json.dumps({"result": result})  # usually JSON string
+            except Exception as e:
+                raise e
+
+            print("Tool result:", result_str)
+
+            # Append function response to history
+            history.append(
+                types.Content(
+                    role="tool",
+                    parts=[
+                        types.Part(
+                            function_response=types.FunctionResponse(
+                                name=func_name,
+                                response={"result": result}  
+                            )
+                        )
+                    ]
+                )
+
+            )
 
